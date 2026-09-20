9edd3c6057044498989d11a1829d5bd2f3a68d27
rizkixp <rizkixp@users.noreply.github.com>
2026-09-20 21:47:26 +0700

    fix(pwa): optimalkan referensi ikon vektor SVG pada manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -21,16 +21,10 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: "any",
       },
       {
-        src: "/icons/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
+        src: "/icons/icon.svg",
+        sizes: "192x192 512x512",
+        type: "image/svg+xml",
         purpose: "maskable",
-      },
-      {
-        src: "/icons/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
       },
     ],
     shortcuts: [
